4b5fcae3cade025d733772c0b577dcb163f74cd5
Stephen Kennedy <stekenn18@gmail.com>
2024-08-05 22:32:16 +0100

    added index page design and js

---
+++ indexscript.js
@@ -0,0 +1,39 @@
+//Used to have a dynamic nav sidebar for smaller screens
+function showSidebar() {
+    const sidebar= document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+
+   let form = document.getElementById('questionairre')
+
+       let username = document.getElementById('name')
+
+       console.log(name)
+
+       let email = document.getElementById('email')
+
+       console.log(email)
+
+       let enjoyed = document.getElementById('enjoyed')
+
+       console.log(enjoyed)
+
+       let enjoyedf = document.getElementById('enjoyedf')
+
+       console.log(enjoyedf)
+
+       let enjoyedc = document.getElementById('enjoyedc')
+
+       console.log(enjoyedc)
+
+       let enjoyedg = document.getElementById('enjoyedg')
+
+       console.log(enjoyedg)
+
+
